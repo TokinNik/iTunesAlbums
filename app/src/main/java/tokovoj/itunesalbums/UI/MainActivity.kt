669d72868ffbacb81d1d0e977c89a09a34b39a53
TokovojNR <TokinNik@yandex.ru>
2019-10-30 22:08:02 +0700

    Доделан поиск и интерфейс

--- app/src/main/java/tokovoj/itunesalbums/UI/MainActivity.kt
+++ app/src/main/java/tokovoj/itunesalbums/UI/MainActivity.kt
@@ -7,6 +7,9 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.ProgressBar
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import tokovoj.itunesalbums.Data.Results
@@ -26,16 +29,35 @@ class MainActivity : AppCompatActivity(), AlbumFragment.OnFragmentInteractionLis
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        net = Network()
         searchButton = findViewById(R.id.search_button)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
-        {
-            searchButton.background.setColorFilter(BlendModeColorFilter(0xD32F2F, BlendMode.SRC_ATOP))
-        }
-        else
-        {
-            searchButton.background.setColorFilter(0xD32F2F, PorterDuff.Mode.MULTIPLY)
-        }
+        searchButton.setOnClickListener{
+            findViewById<ProgressBar>(R.id.search_progressBar).visibility = View.VISIBLE
+            it.visibility = View.GONE
+            net.searchAlbums(findViewById<EditText>(R.id.search_editText).text.toString(),
+            object: SearchAlbumsCallback
+            {
+                private fun selector(it: Results): String = it.collectionName
+
+                override fun onComplete(count: Int, items: List<Results>)
+                {
+                    Log.d("MAIN", "onComplete: $count")
+                    list = items.sortedBy {selector(it)}
+                    findViewById<ProgressBar>(R.id.search_progressBar).visibility = View.GONE
+                    it.visibility = View.VISIBLE
+                    albumsRecycler.adapter = AlbumsRecyclerViewAdapter(list, albumListListener)
+                }
+                override fun onCompleteError(code: Int)
+                {
+                    Log.d("MAIN", "ocCompleteError: $code")
+                }
+
+                override fun onFailture()
+                {
+                    Log.d("MAIN", "ocFailture: ")
+                }
+
+            })}
 
         albumListListener = object : OnAlbumsListIneraxtionListener
         {
@@ -45,27 +67,6 @@ class MainActivity : AppCompatActivity(), AlbumFragment.OnFragmentInteractionLis
             }
         }
 
-        net = Network()
-        net.searchAlbums("Eminem", object: SearchAlbumsCallback
-        {
-            override fun onComplete(albumsCount: Int, albums: List<Results>)
-            {
-                Log.d("MAIN", "onComplete: $albumsCount")
-                list = albums
-                albumsRecycler.adapter = AlbumsRecyclerViewAdapter(list, albumListListener)
-            }
-
-            override fun onCompleteError(code: Int)
-            {
-                Log.d("MAIN", "ocCompleteError: $code")
-            }
-
-            override fun onFailture()
-            {
-                Log.d("MAIN", "ocFailture: ")
-            }
-
-        })
 
         albumsRecycler = findViewById(R.id.albums_recyclerView)
         albumsRecycler.layoutManager = GridLayoutManager(this, 1)
@@ -74,6 +75,7 @@ class MainActivity : AppCompatActivity(), AlbumFragment.OnFragmentInteractionLis
     private fun setAlbumFragment(results: Results)
     {
         albumsRecycler.visibility = View.GONE
+        findViewById<LinearLayout>(R.id.search_linearLayout).visibility = View.GONE
         supportFragmentManager.beginTransaction()
             .add(R.id.container, AlbumFragment(results), AlbumFragment.TAG)
             .commit()
@@ -84,6 +86,7 @@ class MainActivity : AppCompatActivity(), AlbumFragment.OnFragmentInteractionLis
                 Log.d("MAIN", "onComplete: $count")
                 val songs: MutableList<Results> = items.toMutableList()
                 songs.removeAt(0)//todo drugoe reshenie???
+                findViewById<ProgressBar>(R.id.songs_progressBar).visibility = View.GONE
                 (supportFragmentManager.findFragmentByTag(AlbumFragment.TAG) as AlbumFragment).setSongsList(songs)//todo privedenie???
             }
 
@@ -105,6 +108,7 @@ class MainActivity : AppCompatActivity(), AlbumFragment.OnFragmentInteractionLis
         if(albumsRecycler.visibility == View.GONE)
         {
             albumsRecycler.visibility = View.VISIBLE
+            findViewById<LinearLayout>(R.id.search_linearLayout).visibility = View.VISIBLE
             supportFragmentManager.findFragmentByTag(AlbumFragment.TAG)?.let{
                 supportFragmentManager.beginTransaction()
                     .remove(it)
